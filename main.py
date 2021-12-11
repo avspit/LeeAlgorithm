434bd35e2e8b958ae514818ed3ad97e1564b2200
Шестаков Антон Владимирович <avshestakov@sberbank.ru>
2021-12-12 00:22:02 +0300

    refactoring

--- main.py
+++ main.py
@@ -22,26 +22,26 @@ def init_auto_field(rows_max, cols_max):
 def init_manual_start_point(field):
     print('Введите координаты клетки СТАРТ по шаблону [НомерСтроки-НомерСтолбца] (например, 1-3)')
     startPoint = input().split(sep='-')
-    field[int(startPoint[0]) - 1][int(startPoint[1]) - 1] = const.start
+    field[int(startPoint[0]) - 1][int(startPoint[1]) - 1] = const.cell_start
     print(field)
     return field
 
 
 def init_auto_start_point(field, rows_max, col_max):
-    field[randrange(rows_max)][randrange(col_max)] = const.start
+    field[randrange(rows_max)][randrange(col_max)] = const.cell_start
     return field
 
 
 def init_manual_finish_point(field):
     print('Введите координаты клетки ФИНИШ [НомерСтроки-НомерСтолбца] (например, 10-5)')
     finishPoint = input().split(sep='-')
-    field[int(finishPoint[0]) - 1][int(finishPoint[1]) - 1] = const.finish
+    field[int(finishPoint[0]) - 1][int(finishPoint[1]) - 1] = const.cell_finish
     print(field)
     return field
 
 
 def init_auto_finish_point(field, rows_max, col_max):
-    field[randrange(rows_max)][randrange(col_max)] = const.finish
+    field[randrange(rows_max)][randrange(col_max)] = const.cell_finish
     return field
 
 
@@ -49,9 +49,9 @@ def wall_checked(wall):
     if len(wall) <= 1:
         return False
     else:
-        pointBefore = [const.wall, const.wall]
+        pointBefore = [const.cell_wall, const.cell_wall]
         for point in wall:
-            if (pointBefore == [const.wall, const.wall]):
+            if (pointBefore == [const.cell_wall, const.cell_wall]):
                 pointBefore[0] = point[0]
                 pointBefore[1] = point[1]
                 continue
@@ -70,9 +70,9 @@ def init_manual_wall(field):
         print('Не удалось построить стену по указанным точкам. Проверьте, что стена не строится по диагонали и задано более 1 точки')
         init_manual_wall(field)
     else:
-        pointBefore = [const.wall, const.wall]
+        pointBefore = [const.cell_wall, const.cell_wall]
         for point in wall:
-            if (pointBefore == [const.wall, const.wall]):
+            if (pointBefore == [const.cell_wall, const.cell_wall]):
                 curr = point.split('-')
                 pointBefore[0] = curr[0]
                 pointBefore[1] = curr[1]
@@ -81,16 +81,16 @@ def init_manual_wall(field):
                 curr = point.split('-')
                 if (pointBefore[0] == curr[0]):
                     for i in range(int(pointBefore[1]), int(curr[1])):
-                        field[int(pointBefore[0]) - 1][int(i) - 1] = const.wall
+                        field[int(pointBefore[0]) - 1][int(i) - 1] = const.cell_wall
                 else:
                     for i in range(int(pointBefore[0]), int(curr[0])):
-                        field[int(i) - 1][int(pointBefore[1]) - 1] = const.wall
+                        field[int(i) - 1][int(pointBefore[1]) - 1] = const.cell_wall
                 pointBefore[0] = curr[0]
                 pointBefore[1] = curr[1]
 
         for point in wall:
             point = point.split('-')
-            field[int(point[0]) - 1][int(point[1]) - 1] = const.wall
+            field[int(point[0]) - 1][int(point[1]) - 1] = const.cell_wall
         print(field)
         return field
 
@@ -102,11 +102,11 @@ def init_auto_wall(field, rows_max, col_max, points_on_wall):
         if turn == 0:
             nextPoint = [prevPoint[0], randrange(col_max)]
             for j in range(prevPoint[1], nextPoint[1]):
-                field[prevPoint[0]][j] = const.wall
+                field[prevPoint[0]][j] = const.cell_wall
         else:
             nextPoint = [randrange(rows_max), prevPoint[1]]
             for j in range(prevPoint[0], nextPoint[0]):
-                field[j][prevPoint[1]] = const.wall
+                field[j][prevPoint[1]] = const.cell_wall
     return field
 
 
@@ -134,55 +134,47 @@ def init_auto_map():
     return field
 
 
-def path_moore_restoration(field):
-    f=1
-
-
-def path_neyman_restoration(field):
-    finish_point = np.where(field == const.finish)
+def path_restoration(field, wave):
+    finish_point = np.where(field == const.cell_finish)
     row = finish_point[0][0]
     col = finish_point[1][0]
     curr = field[row, col]
     before = curr
-    while curr != const.start:
-        for delta in const.neyman_wave:
+    while curr != const.cell_start:
+        for delta in wave:
             r, c, success = calc_index(row, col, delta)
             if success:
                 curr = field[r, c]
-                if curr == const.start:
+                if curr == const.cell_start:
                     break
-                elif (curr == before-1 or before == const.finish) and curr != const.start and curr != const.finish and curr != const.wall and curr != 0:
-                    field[r, c] = const.path
+                elif (curr == before-1 or before == const.cell_finish) and curr != const.cell_start and curr != const.cell_finish and curr != const.cell_wall and curr != const.cell_empty:
+                    field[r, c] = const.cell_path
                     before = curr
                     break
         row = r
         col = c
 
 
-def wave_moore_propagation(field):
-    f=1
-
-
 def calc_index(row, col, delta):
     r = row + delta[0]
     c = col + delta[1]
-    return r, c, r >= 0 and c >= 0 and r < const.field_rows and c < const.field_cols
+    return r, c, r >= const.cell_empty and c >= const.cell_empty and r < const.field_rows and c < const.field_cols
 
 
-def wave_neyman_arround(field, row, col, step):
+def wave_arround(field, row, col, step, wave):
     finish = False
-    for delta in const.neyman_wave:
+    for delta in wave:
         r, c, success = calc_index(row, col, delta)
         if success:
             curr_val = field[r, c]
-            if curr_val == 0:
+            if curr_val == const.cell_empty:
                 field[r, c] = step + 1
-            elif curr_val == const.finish:
+            elif curr_val == const.cell_finish:
                 finish = True
     return finish
 
 
-def wave_neyman_propagation(field):
+def wave_propagation(field, wave):
     can_wave = True
     finish = False
     step = 1
@@ -191,29 +183,23 @@ def wave_neyman_propagation(field):
             for col in range(const.field_cols):
                 curr = field[row, col]
                 if curr == step:
-                    finish = wave_neyman_arround(field, row, col, step)
+                    finish = wave_arround(field, row, col, step, wave)
                     if finish: break
             if finish: break
         step += 1
-        can_wave = (0 in field) and not finish
+        can_wave = (const.cell_empty in field) and not finish
 
 
-def run_lee_moore(field):
-    wave_moore_propagation(field)
-    path_moore_restoration(field)
-    print(field)
+def run_lee_algorithm(field, wave):
+    wave_propagation(field, wave)
+    path_restoration(field, wave)
 
 
-def run_lee_neyman(field):
-    wave_neyman_propagation(field)
-    path_neyman_restoration(field)
-
-
-def run_lee(field, wave_type="n"):
+def run_lee(field, wave_type='n'):
     if (wave_type == 'm'):
-        run_lee_moore(field)
+        run_lee_algorithm(field, const.moore_wave)
     else:
-        run_lee_neyman(field)
+        run_lee_algorithm(field, const.neyman_wave)
     print(field)
 
 
@@ -221,19 +207,19 @@ def prepare_table_for_colouring(field):
     for row in range(const.field_rows):
         for col in range(const.field_cols):
             curr = field[row][col]
-            if (curr != const.start and curr != const.finish and curr != const.wall and curr != const.path):
-                field[row][col] = 0
+            if (curr != const.cell_start and curr != const.cell_finish and curr != const.cell_wall and curr != const.cell_path):
+                field[row][col] = const.cell_empty
     return field
 
 
 def prepare_table_colours(field):
     field = prepare_table_for_colouring(field)
     colours = field.astype(np.str)
-    colours[colours == str(const.start)] = "#ADFF2F"
-    colours[colours == str(const.finish)] = "#ADFF2F"
-    colours[colours == str(const.wall)] = "#FAEBD7"
-    colours[colours == str(0)] = "w"
-    colours[colours == str(const.path)] = "#FFB6C1"
+    colours[colours == str(const.cell_start)] = const.cell_start_color
+    colours[colours == str(const.cell_finish)] = const.cell_finish_color
+    colours[colours == str(const.cell_wall)] = const.cell_wall_color
+    colours[colours == str(const.cell_empty)] = const.cell_empty_color
+    colours[colours == str(const.cell_path)] = const.cell_path_color
     return colours
 
 
